2ee891a1018b949902f2afb58cbf857084ac0f98
chauhanshivang85@gmail.com <chauhanshivang85@gmail.com>
2023-10-10 19:59:37 +0530

    With Fav

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+// import css from '@rollup/plugin-css';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // [css()],
   
   build: {
     // Configure manual chunks to separate Bootstrap CSS
