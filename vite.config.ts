5c6e663bbd307f3295e081f53f27f4fcfec0114a
chauhanshivang85@gmail.com <chauhanshivang85@gmail.com>
2023-10-10 17:51:09 +0530

    Commiting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  build: {
+    // Configure manual chunks to separate Bootstrap CSS
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'bootstrap-css': ['bootstrap/dist/css/bootstrap.css'],
+        },
+      },
+    },
+  },
 })
